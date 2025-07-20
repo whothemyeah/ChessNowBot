1360b283198118bf455a89e7b8cc9abcbeb71821
whothemyeah <schedupost@gmail.com>
2025-07-20 23:58:31 +0000

    updates

--- server/prisma/migrations/20250720234511_init/migration.sql
+++ server/prisma/migrations/20250720234511_init/migration.sql
@@ -1,20 +1,22 @@
 -- CreateTable
 CREATE TABLE "UserProfile" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "fullName" TEXT NOT NULL,
     "username" TEXT,
     "verified" BOOLEAN NOT NULL DEFAULT false,
     "avatarURL" TEXT,
-    "lastLogin" DATETIME,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "lastLogin" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserProfile_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "PlayedGame" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "timerEnabled" BOOLEAN NOT NULL,
     "timerInit" INTEGER NOT NULL,
     "timerIncrement" INTEGER NOT NULL,
@@ -22,12 +24,12 @@ CREATE TABLE "PlayedGame" (
     "resolution" TEXT NOT NULL,
     "winner" TEXT,
     "gameMode" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
     "whitePlayerID" INTEGER NOT NULL,
     "blackPlayerID" INTEGER NOT NULL,
-    CONSTRAINT "PlayedGame_whitePlayerID_fkey" FOREIGN KEY ("whitePlayerID") REFERENCES "UserProfile" ("id") ON DELETE RESTRICT ON UPDATE RESTRICT,
-    CONSTRAINT "PlayedGame_blackPlayerID_fkey" FOREIGN KEY ("blackPlayerID") REFERENCES "UserProfile" ("id") ON DELETE RESTRICT ON UPDATE RESTRICT
+
+    CONSTRAINT "PlayedGame_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -44,3 +46,9 @@ CREATE INDEX "PlayedGame_whitePlayerID_idx" ON "PlayedGame"("whitePlayerID");
 
 -- CreateIndex
 CREATE INDEX "PlayedGame_blackPlayerID_idx" ON "PlayedGame"("blackPlayerID");
+
+-- AddForeignKey
+ALTER TABLE "PlayedGame" ADD CONSTRAINT "PlayedGame_whitePlayerID_fkey" FOREIGN KEY ("whitePlayerID") REFERENCES "UserProfile"("id") ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+-- AddForeignKey
+ALTER TABLE "PlayedGame" ADD CONSTRAINT "PlayedGame_blackPlayerID_fkey" FOREIGN KEY ("blackPlayerID") REFERENCES "UserProfile"("id") ON DELETE RESTRICT ON UPDATE RESTRICT;
